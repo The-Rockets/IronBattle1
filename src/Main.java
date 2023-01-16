2f755e79fd9b3cf75d0e943cb31fcab5cfccb32d
Pere <pere.alavedra@outlook.com>
2023-01-16 21:06:01 +0100

    red final message

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,7 @@ public class Main {
         Warrior warrior1 = new Warrior("Thor ");
         Wizard wizard1 = new Wizard("jaina ");
 
-        int hpWarrior = warrior1.getHp();
-        int hpWizzard = wizard1.getHp();
         int round = 1;
-        boolean segundabatalla = false;
-
 
         System.out.println(warrior1);
         System.out.println(wizard1);
@@ -29,10 +25,11 @@ public class Main {
             System.out.println(" Wizard HP: " + wizard1.getHp());
         }
         if(warrior1.isAlive()){
-            System.out.println(warrior1.getName()+" has won the battle");
+            System.err.println(warrior1.getName()+" has won the battle");
         } else if (wizard1.isAlive()) {
-            System.out.println(wizard1.getName()+" has won the battle");
-        }else{//aqui deberia empezar el desempate
+            System.err.println(wizard1.getName()+" has won the battle");
+        }else{
+            //aqui deberia empezar el desempate
             round=0;
             battle(warrior1,wizard1,round);
         }
